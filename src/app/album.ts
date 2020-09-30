643a499150c395da41bbd68f0cfc55007252ec04
Lehlohonolo <lehlohonolom@hopeinstitute.co.za>
2020-09-30 06:49:28 +0200

    Using Interfaces to Describe Data

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,8 +1,10 @@
 import { Track } from './track';
 
 export interface Album {
-    name: string,
-    releaseDate: string,
-    coverImage:  string,
+    
+    name: string;
+    releaseDate: string;
+    coverImage:  string;
     tracks: Track[]
+
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Album } from './album';
 import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
   private _albumUrl="../assets/album.json";
+  Album
 
   constructor(private _http: Http, private response) { }
 
@@ -13,8 +15,8 @@ export class ProductService {
 
   }
 
-  getAlbum(id:number){
+  getAlbum(id:number): Observable<Album>{
     return this._http.get(this._albumUrl).map(( response )=> 
-    response.json());
+    <Album>response.json());
   }
 }
