893306d8943e9f5d45d8850d0a2310b9f9664181
Lehlohonolo <lehlohonolom@hopeinstitute.co.za>
2020-09-30 14:47:34 +0200

    Displaying a List of Products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,13 +3,22 @@ import { Http, Response } from '@angular/http';
 import { Album } from './album';
 import 'rxjs/add/operator/map';
 import {Observable} from 'rxjs/Observable';
+import { Product }from './product';
 
 @Injectable()
 export class ProductService {
   private _albumUrl="../assets/album.json";
-  Album
+  Album;
+  private _productsUrl = '../assets/products.json' ; 
+  Product;
 
-  constructor(private _http: Http, private response) { }
+  constructor(private _http: Http, private response:Response) { }
+
+  getProducts() : Observable<Product[]>{
+    this._http.get(this._productsUrl).map((response) =>
+    <Product[]>response.json())
+     return ;
+  }
 
   get(){
 
